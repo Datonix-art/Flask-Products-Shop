69c333158bb160f5ac93d8bf63e50383d86a5ce6
datonix.jsx <davitlezhava1@gmail.com>
2024-07-09 15:07:25 +0400

    commited

--- store/shop/forms.py
+++ store/shop/forms.py
@@ -1,20 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import RadioField, IntegerField, SubmitField, StringField, EmailField
+from wtforms import IntegerField, SubmitField, StringField, EmailField
 from wtforms.validators import InputRequired, Length, Optional
 from flask_babel import lazy_gettext
 
 class BuyingForm(FlaskForm):
-  product_color = RadioField(lazy_gettext('Colours:'), choices=['', ''],validators=[InputRequired()])
-  product_amount = IntegerField()
   Buy = SubmitField(lazy_gettext('Buy Now'))
 
 
 class CheckoutForm(FlaskForm):
-  first_name = StringField('First Name*', validators=[InputRequired(), Length(min=3, max=15)])
-  company_name = StringField('Company Name', validators=[InputRequired(), Length(min=5, max=20)])
-  street_address = StringField('Street address*', validators=[InputRequired(), Length(min=10, max=30)])
-  apartment = StringField('Apartment, floor, etc. (optional)', validators=[Optional()])
-  town_city = StringField('Town/City*', validators=[InputRequired()])
-  number = IntegerField('Phone Number*', validators=[InputRequired()])
-  email = EmailField('Email*', validators=[InputRequired()])
-  place_order = SubmitField('Place Order')
+  first_name = StringField(lazy_gettext('First Name*'), validators=[InputRequired(), Length(min=3, max=15)])
+  company_name = StringField(lazy_gettext('Company Name'), validators=[InputRequired(), Length(min=5, max=20)])
+  street_address = StringField(lazy_gettext('Street address*'), validators=[InputRequired(), Length(min=10, max=30)])
+  apartment = StringField(lazy_gettext('Apartment, floor, etc. (optional)'), validators=[Optional()])
+  town_city = StringField(lazy_gettext('Town/City*'), validators=[InputRequired()])
+  number = IntegerField(lazy_gettext('Phone Number*'), validators=[InputRequired()])
+  email = EmailField(lazy_gettext('Email*'), validators=[InputRequired()])
+  place_order = SubmitField(lazy_gettext('Place Order'))
