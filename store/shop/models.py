198c904097800b5bc22f84069edece8f3c1d6931
datonix.jsx <davitlezhava1@gmail.com>
2024-07-11 17:22:24 +0400

    commited

--- store/shop/models.py
+++ store/shop/models.py
@@ -24,6 +24,7 @@ class ProductModel(db.Model, BaseModel):
     price = db.Column(db.Integer, nullable=False, default=1000)
     amount = db.Column(db.Integer, nullable=False, default=1)
     carts = db.relationship('CartModel', back_populates='product')
+    favorites = db.relationship('FavoriteProductModel', back_populates = 'product')
 
     def __repr__(self):
         return f'id: {self.id} - Name: {self.name} - Rs. {self.price}'
@@ -41,3 +42,13 @@ class OrderModel(db.Model, BaseModel):
     email = db.Column(db.String(50), nullable=False)
     total_price = db.Column(db.Integer, nullable=False)
     created_at = db.Column(db.DateTime, default=func.now())
+
+    def __repr__(self):
+        return f'id:{self.id} ,user id:{self.user_id}, number:{self.number}, email: {self.email}, total price: {self.total_price}, create: {self.created_at}'
+
+class FavoriteProductModel(db.Model, BaseModel):
+    __tablename__ = 'favorites'
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    product = db.relationship('ProductModel', back_populates='favorites')
--- store/shop/routes.py
+++ store/shop/routes.py
@@ -1,7 +1,7 @@
 import os
 from flask import Blueprint, jsonify, redirect, render_template, flash, current_app, request, url_for
 from flask_login import login_required, current_user
-from store.shop.models import OrderModel, ProductModel, db, CategoryModel
+from store.shop.models import OrderModel, ProductModel, db, CategoryModel, FavoriteProductModel
 from store.shop.forms import BuyingForm, CheckoutForm
 from store.cart.models import CartModel
 from datetime import datetime, timedelta
@@ -72,6 +72,42 @@ def all_products(page_id):
 
     all_products = ProductModel.query.paginate(per_page=12, page=page_id)
     return render_template('base.html', products = all_products)
+
+@shop.route('/add_to_favorite/<int:product_id>', methods=['GET', 'POST'])
+@login_required
+def add_to_favorite(product_id):
+    product = FavoriteProductModel.query.filter_by(product_id=product_id, user_id=current_user.id).first()
+    if product:
+        flash('Product is already added to the favorites', 'danger')
+    else:
+        favorite_product = FavoriteProductModel(product_id=product_id, user_id=current_user.id)
+        favorite_product.create()
+        flash('Succesfully added product to favorites', 'success')
+    return redirect(url_for('shop.favorite_products'))   
+    
+@shop.route('/favorite_products')
+@login_required
+def favorite_products():
+    favorite_products = FavoriteProductModel.query.filter_by(user_id=current_user.id).all()
+    return render_template('base.html', favorite_products=favorite_products)
+
+@shop.route('/delete_from_favorites/<int:product_id>')
+@login_required
+def delete_from_favorites(product_id):
+    product = FavoriteProductModel.query.filter_by(product_id=product_id).first()
+    db.session.delete(product)
+    db.session.commit()
+    flash('Succesfully deleted product from favorites', 'success')
+    return redirect(url_for('shop.favorite_products'))
+
+@shop.route('/clear_favorites', methods=['GET', 'POST'])
+@login_required
+def clear_favorites():
+    products = FavoriteProductModel.query.filter_by(user_id=current_user.id).all()
+    for product in products:
+        product.delete()
+    flash('Favorites cleared succesfully', 'success')
+    return redirect(url_for('shop.favorite_products'))
 
 @shop.route('/product_details/<int:id>', methods=['GET', "POST"])
 def inner_product(id):
